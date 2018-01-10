8afc09031196441fb3a8eda1a43fc4478e2231b2
Aleson Alves de Souza <aleson.a.s@gmail.com>
2018-01-10 16:41:31 -0200

    java pocs para recursos inutei em android

---
+++ InsertCopyRigth/src/packageDefault/domain/ResourceType.java
@@ -0,0 +1,7 @@
+package packageDefault.domain;
+
+enum ResourceType {
+	MEDIA,
+	CLASS,
+	DIR
+}
---
+++ ReplacePrintTrace/src/replace/domain/Directory.java
@@ -0,0 +1,13 @@
+package replace.domain;
+
+public class Directory {
+	String path;
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
